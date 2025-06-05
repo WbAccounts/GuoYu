9ff4ba6dfb5b426267f5406ee2fd0d60f604de7b
WbAccounts <85229973+WbAccounts@users.noreply.github.com>
2025-06-05 09:57:32 +0000

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+
+std::map<int, std::string> m_map = {{2, "abc"},
+                                    {3, "def"},
+                                    {4, "ghi"},
+                                    {5, "jkl"},
+                                    {6, "mno"},
+                                    {7, "pqrs"},
+                                    {8, "tuv"},
+                                    {9, "wxyz"}};
+
+
+
+int main () {
+    return 0;
+}
